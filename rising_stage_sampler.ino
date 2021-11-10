e2ba0efa5a98507467778127acbc44540c0feade
HunterGleason <Hunter.Gleason@gov.bc.ca>
2021-11-10 11:49:32 -0800

    Added water temp sensor to Pin 6, now using a parameter file to define sample to avg (N) and filename of CSV log file

--- rising_stage_sampler.ino
+++ rising_stage_sampler.ino
@@ -20,23 +20,25 @@
 #include "RTClib.h" //Needed for communication with Real Time Clock
 #include <SPI.h>//Needed for working with SD card
 #include <SD.h>//Needed for working with SD card
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <CSV_Parser.h>//Needed for parsing CSV data 
 
 
 
-RTC_PCF8523 rtc; //Instantiate PCF8523 RTC
-
-File dataFile;//instantiate a logging file
-
+RTC_PCF8523 rtc; // PCF8523 RTC
+File dataFile;// Logging file
 
 //Define Pins
-int led = 13; // Pin 13 LED
-int donePin = 5; // TPL5110 done pin.
-int h2oSet = 10; // Power set pin for H2O level sensor
-const int h2oUnset = 11; // Power unset pin for H2O level sensor
+const byte led = 13; // Pin 13 LED
+const byte donePin = 5; // TPL5110 done pin.
+const byte h2oSet = 10; // Power set pin for H2O level sensor
+const byte h2oUnset = 11; // Power unset pin for H2O level sensor
 const byte H2O_LEVL_PIN = A0; //Analog pin for reading liquid level sensor output
 const byte TURB_PIN = A1; //Analog pin for reading turbidity sensor output
-const int TURB_SWITCH = 12; //Pin for switching power to turbidity sensor using 4N37 optocoupler
+const byte TURB_SWITCH = 12; //Pin for switching power to turbidity sensor using 4N37 optocoupler
 const byte chipSelect = 4; //Chip select pin for MicroSD breakout
+const byte tempPin = 6;
 
 //Define Global constants, change as required
 const float RANGE = 5000.0; // Depth measuring range 5000mm (for water).
@@ -45,9 +47,15 @@ const float DENSITY_WATER = 1.00;  // Pure water density.
 const float H2O_VREF = 3300.0; //Reference voltage, 3.3V for Adalogger M0, measure with multimeter for better accuracy
 const float TURB_VREF = 5000.0; //Reference voltage for turbidity sensor, measure with multimeter for better accuracy
 const int ANLG_RES = 12; //Desired analog resolution 10,12 or 16.
-const float MAX_ANALOG_VAL = 4096.0; // Maximum analog value at specified ADC resolution.
-const String filename = "DATATST1.TXT";//Desired name for logfile !!!must be less than 8 char!!!
-const int N = 5; //Number of sensor readings to average.
+const float MAX_ANALOG_VAL = 4096.0; // Maximum analog value at 12-bit ADC resolution.
+char **filename;//Desired name for data file !!!must be less than equal to 8 char!!!
+char **N_str; //Number of ultrasonic reange sensor readings to average.
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(tempPin);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
 
 /*Function for converting voltage read from turbidity sensor 'turb_volt' to NTU units (from calibration),
    change to observed calbration for setup. With a multimeter adjust PCB potentiometer so that output voltage 
@@ -149,6 +157,23 @@ void setup() {
   //Set analog resolution to ANLG_RES bit
   analogReadResolution(ANLG_RES);
 
+  //Set paramters for parsing the parameter file
+  CSV_Parser cp(/*format*/ "ss", /*has_header*/ true, /*delimiter*/ ',');
+
+  //Read the parameter file off SD card (params.csv), 1/4-sec flash means file is not available
+  while (!cp.readSDfile("/params.csv"))
+  {
+    digitalWrite(led, HIGH);
+    delay(250);
+    digitalWrite(led, LOW);
+    delay(250);
+  }
+
+  //Read values from SNOW_PARAM.TXT into global varibles
+  filename = (char**)cp["filename"];
+  N_str = (char**)cp["N"];
+  int N = String(N_str[0]).toInt();
+
   //Get current logging time from RTC
   DateTime now = rtc.now();
 
@@ -185,11 +210,29 @@ void setup() {
   //Unset 5V power to turbidity sensor
   digitalWrite(TURB_SWITCH, LOW);
 
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  // We use the function ByIndex, and as an example get the temperature from the first (and only) sensor only.
+  float tempC = sensors.getTempCByIndex(0);
+  
+
   //Assemble a data string for logging to SD, with time and average level NTU values
-  String datastring = String(now.year()) + ":" + String(now.month()) + ":" + String(now.day()) + " " + String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second()) + "," + String(depth_mm) + " mm ," + String(turb_ntu) + "NTU";
+  String datastring = String(now.year()) + ":" + String(now.month()) + ":" + String(now.day()) + " " + String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second()) + "," + String(depth_mm) + "," + String(turb_ntu) + "," + String(tempC);
+
+  //Write header if first time writing to the file 
+  if(!SD.exists(filename[0]))
+  {
+    dataFile = SD.open(filename[0], FILE_WRITE);
+    if (dataFile)
+    {
+      dataFile.println("datetime,h2o_depth_mm,turbidity_ntu,h2o_temp_degC");
+      dataFile.close();
+    }
+
+  }
+  
 
   //Write datastring and close logfile on SD card
-  dataFile = SD.open(filename, FILE_WRITE);
+  dataFile = SD.open(filename[0], FILE_WRITE);
   if (dataFile)
   {
     dataFile.println(datastring);
